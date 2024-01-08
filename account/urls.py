9c41c3f39fc7e479c011b53a56210c0f364c9344
thanh hai <109170970+duongthanhhaii@users.noreply.github.com>
2024-01-09 00:59:44 +0700

    Add files via upload

---
+++ account/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.views import LogoutView
+from .views import *
+
+urlpatterns = [
+    path('register', RegisterView.as_view(), name='register'),
+    path('login', LoginView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
+    path('update_profile/<int:id>', update_profile, name = "update_profile")
+
+    # path('token_send', token_send, name='token_send'),
+    # path('emailActivate/<uidb64>/<token>', activate, name='emailActivate')
+]

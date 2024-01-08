cf52aeb59e8b57bef265408a315ce8b4c0f28cd9
thanh hai <109170970+duongthanhhaii@users.noreply.github.com>
2024-01-09 00:55:53 +0700

    Delete cinemaa directory

--- cinemaa/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import Movie
-
-class MovieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = ['movie_id', 'title', 'description', 'trailer', 'poster', 'age', 'rating', 'cost', 'status', 'released_year', 'director', 'producer', 'language', 'genre']
-

13d7b15fc2a560d9df18624374f8148fe4b1899c
thanh hai <109170970+duongthanhhaii@users.noreply.github.com>
2024-01-10 02:21:36 +0700

    Update urls.py

--- account/urls.py
+++ account/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('register', RegisterView.as_view(), name='register'),
     path('login', LoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
-    path('update_profile/<int:id>', update_profile, name = "update_profile")
+    path('update_profile/<int:id>', update_profile, name = "update_profile"),
+    path('change_password', change_password, name = 'change_password')
 
     # path('token_send', token_send, name='token_send'),
     # path('emailActivate/<uidb64>/<token>', activate, name='emailActivate')

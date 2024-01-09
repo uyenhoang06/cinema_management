9c2b6114bfbb5688ca693ca961c2b373c47db564
thanh hai <109170970+duongthanhhaii@users.noreply.github.com>
2024-01-10 00:31:53 +0700

    Delete movie directory

--- movie/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-from django.db.models.signals import post_migrate
-
-
-class MovieConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'movie'
-
-
